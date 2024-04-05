292c84d9bd9432e551bb61806ea51523ed9196b8
Lunginno <sifumbalungile@gmail.com>
2024-04-05 09:19:51 +0200

    loops

---
+++ forbreak.js
@@ -0,0 +1,11 @@
+//let's create a for...loop with break statement to list the incrementation
+
+for (i = 1; i <= 10; i++)
+{
+    //break condition with a limiter
+    if (i == 7)
+    {
+        break;
+    }
+    console.log(i);
+}
